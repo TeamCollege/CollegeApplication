eafb06ca1d60734e5d0746c2bfb64c7e1cf8c0da
vidhyasrim <vidhyasrim@HDRVELOCITY51.Virtusa.com>
2019-10-22 10:54:15 +0530

    app

--- VirtusaCollegeApplicationWeb/src/com/virtusa/controller/ApplicantControllerServlet.java
+++ VirtusaCollegeApplicationWeb/src/com/virtusa/controller/ApplicantControllerServlet.java
@@ -1,19 +1,18 @@
 package com.virtusa.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.ParseException;
-import java.util.logging.Logger;
-
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.virtusa.entities.Applicant;
+import com.virtusa.helper.FactoryApplicantService;
 import com.virtusa.model.ApplicantModel;
-import com.virtusa.service.ApplicantServiceImpl;
+import com.virtusa.service.ApplicantService;
 
 /**
  * Servlet implementation class ApplicantControllerServlet
@@ -21,7 +20,7 @@ import com.virtusa.service.ApplicantServiceImpl;
 @WebServlet("/ApplicantControllerServlet")
 public class ApplicantControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	static Logger logger = Logger.getLogger(ApplicantController.class);
+	private ApplicantService applicantService=null;
 
     /**
      * Default constructor. 
@@ -29,31 +28,32 @@ public class ApplicantControllerServlet extends HttpServlet {
     public ApplicantControllerServlet() {
     	super();
     }
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    	super.init(config);
+    	this.applicantService=FactoryApplicantService.createApplicantService();
+    }
  
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ApplicantModel applicantModel=new ApplicantModel();
-		PrintWriter out=null;
-		try {
-			out = response.getWriter();
-			applicantModel.setFirstName(request.getParameter("firstName"));
-			applicantModel.setLastName(request.getParameter("lastName"));
-			applicantModel.setPhoneNumber(request.getParameter("phoneNumber"));
-			applicantModel.setDateOfBirth(request.getParameter("dateOfBirth"));
-			applicantModel.setEmail(request.getParameter("email"));
-			applicantModel.setTenthPercentage(Integer.parseInt(request.getParameter("tenthPrecentage")));
-			applicantModel.setInterPercentage(Integer.parseInt(request.getParameter("interPrecentage")));
-			ApplicantServiceImpl applicantServiceImpl = new ApplicantServiceImpl();
-			String result = applicantServiceImpl.storeApplicantService(applicantModel);
-			if(result.contentEquals("success")) {
-				out.println("Application Sucessfully Submitted");
-				out.println("Your Application Number is: "+request.getParameter("applicationNumber"));
-			}
-			} catch (ParseException | NumberFormatException e) {
-				logger.info(e);
-			}
+		String action=request.getParameter("action");
+		if(action.contentEquals("view")) {
+		List<ApplicantModel> applicantModelList=applicantService.retrieveApplicants();
+		request.setAttribute("applicantModelList", applicantModelList);
+		
+		if(!applicantModelList.isEmpty()) {
+			
+			RequestDispatcher dispatcher=
+					request.getRequestDispatcher("ApplicationSuccessful.jsp");
+			dispatcher.forward(request,response);
+		}else {
+			RequestDispatcher dispatcher=
+					request.getRequestDispatcher("ApplicationUnsuccessful.jsp");
+			dispatcher.forward(request,response);
+		}
 	}
-
+}
 }
